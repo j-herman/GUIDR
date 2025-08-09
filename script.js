6c6ac85eb1ac3eaabde84c25a1baaa4a8e304a51
jessica.herman <jessica.herman@nps.edu>
2025-08-09 13:59:06 -0700

    api key

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const options = {
     // Required: API key
-    key: 'PsLAtXpsPTZexBwUkO7Mx5I', // REPLACE WITH YOUR KEY !!!
+    key: 'NFY66hfWKinLAFUnjfgyDbNX6OShlMmI', // REPLACE WITH YOUR KEY !!!
 
     // Put additional console output
     verbose: true,
